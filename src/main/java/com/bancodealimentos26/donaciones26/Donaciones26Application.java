a8fd4bfb7844af4bac2ffd2463e7410b9f67f181
enzo-sash <dmaghimilyc@gmail.com>
2026-04-09 21:53:20 -0500

    proyecto de spring boot inicial

---
+++ src/main/java/com/bancodealimentos26/donaciones26/Donaciones26Application.java
@@ -0,0 +1,13 @@
+package com.bancodealimentos26.donaciones26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Donaciones26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Donaciones26Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bancodealimentos26/donaciones26/Donaciones26ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancodealimentos26.donaciones26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Donaciones26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
